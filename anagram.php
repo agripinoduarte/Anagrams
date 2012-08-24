2a119b27f4edec9ea215356062303c6dafac0d6c
Agripino Duarte <agripinoduarte@gmail.com>
2012-08-23 23:17:47 -0400

    psr-2 na classe Anagram

--- anagram.php
+++ anagram.php
@@ -8,8 +8,7 @@ class Anagram
 
 	public function __construct($word)
 	{
-		for($i = 0; $i < strlen($word); $i++)
-		{
+		for($i = 0; $i < strlen($word); $i++) {
 			$this->letters[] = $word[$i];
 		}
 	}
@@ -19,8 +18,7 @@ class Anagram
 		if(count($this->letters) > $this->maxWord)
 			return false;
 		
-		foreach($this->letters as $k => $l)
-		{
+		foreach($this->letters as $k => $l) {
 			$this->used[$k] = 1;
 			$this->deep($l, 0);
 			unset($this->used[$k]);
@@ -31,20 +29,16 @@ class Anagram
 
 	public function deep($word, $pos)
 	{
-		if($pos >= $this->maxWord-1)
-		{
-			if(!in_array($word, $this->words))
-			{
+		if($pos >= $this->maxWord-1) {
+			if(!in_array($word, $this->words)) {
 				$this->words[] = $word;
 			}
 
 			return;
 		}
 
-		for($n = 0; $n < $this->maxWord; $n++)
-		{
-			if(!isset($this->used[$n]))
-			{
+		for($n = 0; $n < $this->maxWord; $n++) {
+			if(!isset($this->used[$n])) {
 				$this->used[$n] = 1;
 				$this->deep($word . $this->letters[$n], $pos+1);
 				unset($this->used[$n]);
